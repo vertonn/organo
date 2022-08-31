ba7d4df7eba42085627eeb87020b1d8c61e733d1
vertonn <vertonndev@gmail.com>
2022-08-31 16:40:22 -0300

    refactor: remove constante times de ...Formulario/index.js e a eleva App.js

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -5,16 +5,6 @@ import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
 
 const Formulario = (props) => {
-  const times = [
-    "",
-    "Programação",
-    "Front-End",
-    "Data Science",
-    "Devops",
-    "Ux e Design",
-    "Mobile",
-    "Inovação e Gestão",
-  ];
 
   const [nome, setNome] = useState("");
   const [cargo, setCargo] = useState("");
@@ -59,7 +49,7 @@ const Formulario = (props) => {
         <ListaSuspensa
           obrigatorio={true}
           label="Time"
-          itens={times}
+          itens={props.times}
           valor={time}
           aoAlterado={(valor) => setTime(valor)}
         />
