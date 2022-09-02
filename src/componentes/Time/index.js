cad85dec313f909fa3c27c24ae954199cc55b047
vertonn <vertonndev@gmail.com>
2022-09-02 12:13:01 -0300

    refactor: adiciona colaborador ao componente Time e cria condição para exibir somente os times com colaborador

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,14 +1,32 @@
-import './Time.css'
+import Colaborador from "../Colaborador";
+import "./Time.css";
 
 const Time = (props) => {
-    return (
-        // const css = {backgroundColor: props.corSecundaria}
-        // style={{ css}}
-        <section className="time" style={{backgroundColor: props.corSecundaria}}>
-            <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
-        </section>
+  return (
+    // const css = {backgroundColor: props.corSecundaria}
+    // style={{ css}}
+    props.colaboradores.length > 0 ? (
+      <section
+        className="time"
+        style={{ backgroundColor: props.corSecundaria }}
+      >
+        <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+        <div className="colaboradores">
+          {props.colaboradores.map((colaborador) => (
+            <Colaborador
+              corDeFundo={props.corPrimaria}
+              key={colaborador.nome}
+              nome={colaborador.nome}
+              cargo={colaborador.cargo}
+              imagem={colaborador.imagem}
+            />
+          ))}
+        </div>
+      </section>
+    ) : (
+      ""
     )
-
-}
+  );
+};
 
 export default Time;
